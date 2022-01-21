fd8d81eb9d61088f084beb98f4ffb69087be4a0f
Johannes Hagglund <johagg17@berlin.hh.se>
2022-01-21 12:52:50 +0100

    Creating tokenizer

--- model/model.py
+++ model/model.py
@@ -3,38 +3,43 @@ from modules import *
 
 
 class BERT(nn.Module):
-   def __init__(self, config):
-       super(BERT, self).__init__()
-       self.embedding = BertEmbeddings(config=config)
-       self.layers = nn.ModuleList([BertEncoder(config=config) for _ in range(config.n_layers)])
-       self.fc = nn.Linear(config.d_s, config.d_s)
-       self.activ1 = nn.Tanh()
-       self.linear = nn.Linear(config.d_s, config.d_s)
-       self.activ2 = gelu
-       self.norm = nn.LayerNorm(config.d_s)
-       self.classifier = nn.Linear(config.d_s, 2)
-       # decoder is shared with embedding layer
-       embed_weight = self.embedding.tok_embed.weight
-       n_vocab, n_dim = embed_weight.size()
-       self.decoder = nn.Linear(n_dim, n_vocab, bias=False)
-       self.decoder.weight = embed_weight
-       self.decoder_bias = nn.Parameter(nn.zeros(n_vocab))
+    def __init__(self, config):
+        super(BERT, self).__init__()
+        d_s = config['d_model']
+        nlayers = config['n_layers']
+                         
+        self.embedding = BertEmbeddings(config=config)
+        self.layers = nn.ModuleList([BertEncoder(config=config) for _ in range(nlayers)])
+        self.fc = nn.Linear(d_s, d_s)
+        self.activ1 = nn.Tanh()
+        self.linear = nn.Linear(d_s, d_s)
+        self.activ2 = gelu
+        self.norm = nn.LayerNorm(d_s)
+        self.classifier = nn.Linear(d_s, 2)
+        
+        # decoder is shared with embedding layer
+        
+        embed_weight = self.embedding.tok_embed.weight
+        n_vocab, n_dim = embed_weight.size()
+        self.decoder = nn.Linear(n_dim, n_vocab, bias=False)
+        self.decoder.weight = embed_weight
+        self.decoder_bias = nn.Parameter(torch.zeros(n_vocab))
 
-   def forward(self, input_ids, segment_ids, masked_pos):
-       output = self.embedding(input_ids, segment_ids)
-       enc_self_attn_mask = get_attn_pad_mask(input_ids, input_ids)
-       for layer in self.layers:
-           output, enc_self_attn = layer(output, enc_self_attn_mask)
-       # output : [batch_size, len, d_model], attn : [batch_size, n_heads, d_mode, d_model]
-       # it will be decided by first token(CLS)
-       h_pooled = self.activ1(self.fc(output[:, 0])) # [batch_size, d_model]
-       logits_clsf = self.classifier(h_pooled) # [batch_size, 2]
+    def forward(self, input_ids, segment_ids, masked_pos):
+        output = self.embedding(input_ids, segment_ids)
+        enc_self_attn_mask = get_attn_pad_mask(input_ids, input_ids)
+        for layer in self.layers:
+            output, enc_self_attn = layer(output, enc_self_attn_mask)
+        # output : [batch_size, len, d_model], attn : [batch_size, n_heads, d_mode, d_model]
+        # it will be decided by first token(CLS)
+        h_pooled = self.activ1(self.fc(output[:, 0])) # [batch_size, d_model]
+        logits_clsf = self.classifier(h_pooled) # [batch_size, 2]
 
-       masked_pos = masked_pos[:, :, None].expand(-1, -1, output.size(-1)) # [batch_size, max_pred, d_model]
+        masked_pos = masked_pos[:, :, None].expand(-1, -1, output.size(-1)) # [batch_size, max_pred, d_model]
 
-       # get masked position from final output of transformer.
-       h_masked = nn.gather(output, 1, masked_pos) # masking position [batch_size, max_pred, d_model]
-       h_masked = self.norm(self.activ2(self.linear(h_masked)))
-       logits_lm = self.decoder(h_masked) + self.decoder_bias # [batch_size, max_pred, n_vocab]
-
-       return logits_lm, logits_clsf
+        # get masked position from final output of transformer.
+        h_masked = torch.gather(output, 1, masked_pos) # masking position [batch_size, max_pred, d_model]
+        h_masked = self.norm(self.activ2(self.linear(h_masked)))
+        logits_lm = self.decoder(h_masked) + self.decoder_bias # [batch_size, max_pred, n_vocab]
+ 
+        return logits_lm, logits_clsf
--- model/modules.py
+++ model/modules.py
@@ -1,25 +1,29 @@
-
-import torch as nn
+import torch
+import torch.nn as nn
 import numpy as np
 import math as m
 
 
 def gelu(x):
-   return x * 0.5 * (1.0 + nn.erf(x / m.sqrt(2.0)))
+    return x * 0.5 * (1.0 + torch.erf(x / m.sqrt(2.0)))
 
 def get_attn_pad_mask(seq_q, seq_k):
-   batch_size, len_q = seq_q.size()
-   batch_size, len_k = seq_k.size()
-   # eq(zero) is PAD token
-   pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
-   return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
+    
+    batch_size, len_q = seq_q.size()
+    batch_size, len_k = seq_k.size()
+    # eq(zero) is PAD token
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
+    return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
 
 
 class PointWiseFeedForwardNet(nn.Module):
     def __init__(self, config):
         super(PointWiseFeedForwardNet, self).__init__()
-        self.fc1 = nn.Linear(config.d_model, config.d_ff)
-        self.fc2 = nn.Linear(config.d_ff, config.d_model)
+        d_ff = config['d_ff'] 
+        d_s = config['d_model']
+        
+        self.fc1 = nn.Linear(d_s, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_s)
 
     def forward(self, x):
         # (batch_size, len_seq, d_model) -> (batch_size, len_seq, d_ff) -> (batch_size, len_seq, d_model)
@@ -27,50 +31,54 @@ class PointWiseFeedForwardNet(nn.Module):
 
 
 class SelfAttention(nn.Module):
-   def __init__(self):
-       super(SelfAttention, self).__init__()
+    
+    def __init__(self, config):
+        super(SelfAttention, self).__init__()
+        self.d_k = config['d_k']
 
-   def forward(self, Q, K, V, attn_mask):
-       scores = nn.matmul(Q, K.transpose(-1, -2)) / np.sqrt(Q.size(-1)) # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
-       scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is one.
-       attn = nn.Softmax(dim=-1)(scores)
-       context = nn.matmul(attn, V)
-       return scores, context, attn
+    def forward(self, Q, K, V, attn_mask):
+        d_k = self.d_k
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k) # scores : [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
+        scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is one.
+        attn = nn.Softmax(dim=-1)(scores)
+        context = torch.matmul(attn, V)
+        
+        return scores, context, attn
 
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, config):
         super(MultiHeadAttention, self).__init__()
-        embed_dim = config.embed_dim
-        num_heads = config.num_heads
+        embed_dim = config['d_model']
+        num_heads = config['n_heads']
 
         assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
         
-        self.d_s = config.dimension_s
+        self.d_s = config['d_model']
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        self.head_dim = embed_dim // num_heads
-
+        
+        self.config = config
         # Stack all weight matrices 1...h together for efficiency
         # Note that in many implementations you see "bias=False" which is optional
-        self.q = nn.Linear(embed_dim, embed_dim * num_heads)
-        self.k = nn.Linear(embed_dim, embed_dim * num_heads)
-        self.v = nn.Linear(embed_dim, embed_dim * num_heads)
+        self.q = nn.Linear(embed_dim, config['d_k'] * num_heads)
+        self.k = nn.Linear(embed_dim, config['d_k'] * num_heads)
+        self.v = nn.Linear(embed_dim, config['d_k'] * num_heads)
 
     def forward(self, q, k, v, mask=None):
-
+        
+        
         res, batch_size = q, q.size(0)
 
-        q_s = self.W_Q(q).view(batch_size, -1, self.num_heads, self.embed_dim).transpose(1,2)  # q_s: [batch_size x n_heads x len_q x d_k]
-        k_s = self.W_K(k).view(batch_size, -1, self.num_heads, self.embed_dim).transpose(1,2)  # k_s: [batch_size x n_heads x len_k x d_k]
-        v_s = self.W_V(v).view(batch_size, -1, self.num_heads, self.embed_dim).transpose(1,2)  # v_s: [batch_size x n_heads x len_k x d_v]
+        q_s = self.q(q).view(batch_size, -1, self.num_heads, self.config['d_k']).transpose(1,2)  # q_s: [batch_size x n_heads x len_q x d_k]
+        k_s = self.k(k).view(batch_size, -1, self.num_heads, self.config['d_k']).transpose(1,2)  # k_s: [batch_size x n_heads x len_k x d_k]
+        v_s = self.v(v).view(batch_size, -1, self.num_heads, self.config['d_k']).transpose(1,2)  # v_s: [batch_size x n_heads x len_k x d_v]
 
-
-        attn_mask = mask.unsqueeze(1).repeat(1, self.n_heads, 1, 1) # attn_mask : [batch_size x n_heads x len_q x len_k]
-        _, values, attention = SelfAttention(Q=q, K=k, V=v, attn_mask=mask)
-        values = values.transpose(1, 2).contiguous().view(batch_size, -1, self.num_heads * self.embed_dim) # context: [batch_size x len_q x n_heads * d_v]
-        output = nn.Linear(self.num_heads * self.embed_dim, self.d_s)(values)
-
+        attn_mask = mask.unsqueeze(1).repeat(1, self.num_heads, 1, 1) # attn_mask : [batch_size x n_heads x len_q x len_k]
+        _, values, attention = SelfAttention(config=self.config)(Q=q, K=k, V=v, attn_mask=mask)
+        values = values.transpose(1, 2).contiguous().view(batch_size, -1, self.num_heads * self.config['d_k']) # context: [batch_size x len_q x n_heads * d_v]
+        output = nn.Linear(self.num_heads * self.config['d_k'], self.d_s)(values)
+ 
         return nn.LayerNorm(self.d_s)(output + res), attention
 
 
@@ -79,23 +87,21 @@ class BertEmbeddings(nn.Module):
     def __init__(self, config):
         super(BertEmbeddings, self).__init__()
     
-        self.word_embed = nn.Embedding(config.wordvocab_size, config.hidden_size)
-        self.segment = nn.Embedding(config.segvocab_size, config.hidden_size)
-        self.age = nn.Embedding(config.agevocab_size, config.hidden_size)
-        self.pos = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.tok_embed = nn.Embedding(config['wordvocab_size'], config['d_model'])
+        self.segment = nn.Embedding(config['n_segments'], config['d_model'])
+      #  self.age = nn.Embedding(config[''], config.hidden_size)
+        self.pos_embed = nn.Embedding(config['maxlen'], config['d_model'])
 
-        self.norm = nn.LayerNorm(config.d_s)
+        self.norm = nn.LayerNorm(config['d_model'])
 
-    def forward(self, word_x, age_x, seg_x, pos_x):
-
-        w = self.word_embed(word_x)
-        a = self.age(age_x)
-        s = self.segment(seg_x)
-        p = self.pos(pos_x)
-
-        embeddings = self.norm(w + a + s + p)
-
-        return embeddings
+    def forward(self, x, seg):
+        
+        seq_len = x.size(1)
+        pos = torch.arange(seq_len, dtype=torch.long)
+        pos = pos.unsqueeze(0).expand_as(x)  # (seq_len,) -> (batch_size, seq_len)
+        
+        embedding = self.tok_embed(x) + self.pos_embed(pos) + self.segment(seg)
+        return self.norm(embedding)
 
 
 class BertEncoder(nn.Module):
@@ -106,12 +112,7 @@ class BertEncoder(nn.Module):
         self.pos_ffn = PointWiseFeedForwardNet(config=config)
 
     def forward(self, enc_inputs, enc_self_attn_mask):
-       enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
-       enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size x len_q x d_model]
-       return enc_outputs, attn
-
-
-
         
-
-
+        enc_outputs, attn = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
+        enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size x len_q x d_model]
+        return enc_outputs, attn
