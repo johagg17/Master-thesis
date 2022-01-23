f4b5d0b4f033bda1ba432b5a1904c8d0a9b6a4f1
johagg17 <johagg17@student.hh.se>
2022-01-23 17:21:02 +0100

    Implementing class for processing data

--- pre-processing/patient_extraction.py
+++ pre-processing/patient_extraction.py
@@ -1,35 +1,90 @@
 
 import pandas as pd
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf,col
 
-class PatientExtractor(object):
+class PatientProcessor(object):
     
-    def __init__(self, **files):
+    def __init__(self, parent_path, **files):
         
+        self.parent_path = parent_path
         self.files = files
-        self.__subject_id_age__()
+        self.pat_data = None
+        self.__process__()
 
-    def __subject_id_age__(self):
-        assert self.files['id_age'],  "Subject_id and age file not included" 
-
-        df_pat = pd.read_csv(self.files['id_age'])
-
-        df_adm = pd.read_csv(self.files['pat_adm'])
-        #subject_age = df_pat[df_pat['subject_id'], df_pat['anchor_age']]
-        #print(subject_age)
+    def __init_spark__(self):
         
-        tot = pd.merge(df_pat, df_adm, on='subject_id')
+        spark = SparkSession \
+            .builder \
+            .appName("Python Spark SQL basic example") \
+            .config("spark.some.config.option", "some-value") \
+            .getOrCreate()
+        return spark
 
-        print(tot)
 
-
+    def __process__(self):
+        spark = self.__init_spark__()
+        parent_path = self.parent_path
         
+        codes_icd_path = parent_path + '/hosp/diagnoses_icd.csv'
+        diagnose_codes_icd = spark.read.load(codes_icd_path, format="csv", sep=",", inferSchema="true", header="true")
+        
+        diagnoses_codes_path = parent_path + '/hosp/d_icd_diagnoses.csv'
+        diagnoses_codes = spark.read.load(diagnoses_codes_path,  format="csv", sep=",", inferSchema="true", header="true")  
+        
+        patients_path = parent_path + '/core/patients.csv'
+        patients = spark.read.load(patients_path,  format="csv", sep=",", inferSchema="true", header="true")  
+        
+        admission_path = parent_path + '/core/admissions.csv'
+        admissions = spark.read.load(admission_path,  format="csv", sep=",", inferSchema="true", header="true")  
+        
+        admissions_ = admissions.withColumnRenamed('subject_id', 'subject_id2')
+        patients_admissions = patients.join(admissions_, patients.subject_id == admissions_.subject_id2, 'inner').drop('subject_id2')
+        
+        diagnoses_codes_ = diagnoses_codes.withColumnRenamed('icd_code', 'code').withColumnRenamed('icd_version', 'version')
 
-
+        patients_diagnoses = diagnose_codes_icd.join(diagnoses_codes_, (diagnose_codes_icd.icd_code == diagnoses_codes_.code) \
+                                                     & (diagnose_codes_icd.icd_version == diagnoses_codes_.version), "inner") \
+        .drop('code', 'version')
+        
+        patients_diagnoses = patients_diagnoses.withColumnRenamed('subject_id', 'sub2').withColumnRenamed('hadm_id', 'h_id')
+        
+        
+        pat_data = patients_admissions.join(patients_diagnoses, ((patients_admissions.subject_id == patients_diagnoses.sub2) & \
+                                                                   (patients_admissions.hadm_id == patients_diagnoses.h_id)) ,\
+                                              'inner').drop('sub2', 'h_id')
+        
+        needed_columns = set(['subject_id', 'anchor_age', 'hadm_id', 'admittime', 'dischtime', 'icd_code', 'icd_version', 'long_title'])
+        
+        columns_ = [c for c in pat_data.columns if c not in needed_columns]
+        
+        new = pat_data.drop(*columns_).na.drop()
+        
+        self.pat_data = new
+    
+    def getData(self):
+        return self.pat_data
+        
+    def write_data(self, path):
+        
+        if path_data is None:
+            return None
+        
+        self.path_data.write.option('header', True).csv(path)
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 if __name__ == "__main__":
     parent_path = 'D:\skolaÅr5\MasterThesis\mimic-iv-1.0'.replace('\\', '/') 
 
-    #print("Parent path: {}".format(parent_path))
 
     patients_f = parent_path + '/core/patients.csv'
     admission_f = parent_path + '/core/admissions.csv'
@@ -43,7 +98,7 @@ if __name__ == "__main__":
         'pat_diagnose_icd': pat_diagnos, 
         'diagnos_meaning': meaning_diagnos,
         }
-    extr = PatientExtractor(**config)
+    extr = PatientProcessor(parent_path, **config)
 
 
 
